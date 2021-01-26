174b1f1fcd68fa12e96759e6658c0fd5c1ba0f21
Seamus <seamusugochukwu@gmail.com>
2021-01-26 11:54:03 +0100

    Initialised npm package.json file and installed nodemon globally and also created app.js with express and ejs view engine

--- modules.js
+++
@@ -1,6 +0,0 @@
-// const xyz = require("./people");
-
-// console.log(xyz.people, xyz.ages);
-
-const os = require("os");
-console.log(os.platform(), os.homedir());
--- people.js
+++
@@ -1,7 +0,0 @@
-const people = ["yoshida", "tablang", "cora", "quota"];
-const ages = [22, 32, 12, 24];
-
-module.exports = {
-  people,
-  ages,
-};
